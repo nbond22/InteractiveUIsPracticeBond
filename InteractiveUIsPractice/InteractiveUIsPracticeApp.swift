109897d5cc2d47acb98200b1d3229487b748b8e9
Nicole Bond <nicbond22@gmail.com>
2024-04-28 11:16:02 -0400

    Initial Commit

---
+++ InteractiveUIsPractice/InteractiveUIsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIsPracticeApp.swift
+//  InteractiveUIsPractice
+//
+//  Created by Scholar on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
